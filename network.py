71031f1a3bb3645acdcd4ab5e052febe952d06b9
Julek <jkluge>
2022-11-19 22:49:50 +0100

    1

--- network.py
+++ network.py
@@ -71,57 +71,90 @@ Poznan ='10.0.0.10'
 
 # Część 4:
 # Wyznaczenie najbardziej optymalnej ścieżki za pomocą algorytmu
-class dane(object):
-  def __init__(self, v, p, d):
-    self.odwiedzony = v
-    self.poprzednik = p
-    self.dystans = d
-def szukajMinimum(tab):
-  min = -1
-  mindist = sys.maxsize
-  for i in range(0, len(macierz)):
-    if ((not tab[i].odwiedzony) and tab[i].dystans < mindist):
-      min = i
-      mindist = tab[i].dystans
-  return min
-def Dijkstra(macierz, start):
-  tab = []
-  for i in range(0, len(macierz)):
-    tab.append(dane(False, -1, sys.maxsize))
-  tab[start].dystans = 0
-  u = start
-  while(u != -1):
-    tab[u].odwiedzony = True
-    for i in range(0, len(macierz)):
-      if (macierz[u][i] > 0 and tab[u].dystans + macierz[u][i] < tab[i].dystans):
-        tab[i].dystans = tab[u].dystans + macierz[u][i]
-        tab[i].poprzednik = u
-    u = szukajMinimum(tab)
-  return tab
+# Python program for Dijkstra's single
+# source shortest path algorithm. The program is
+# for adjacency matrix representation of the graph
+class Graph():
+
+	def __init__(self, vertices):
+		self.V = vertices
+		self.graph = [[0 for column in range(vertices)]
+					for row in range(vertices)]
+
+	def printSolution(self, dist):
+		print("Vertex \t Distance from Source")
+		for node in range(self.V):
+			print(node, "\t\t", dist[node])
+
+	# A utility function to find the vertex with
+	# minimum distance value, from the set of vertices
+	# not yet included in shortest path tree
+	def minDistance(self, dist, sptSet):
+
+		# Initialize minimum distance for next node
+		min = 1e7
+
+		# Search not nearest vertex not in the
+		# shortest path tree
+		for v in range(self.V):
+			if dist[v] < min and sptSet[v] == False:
+				min = dist[v]
+				min_index = v
+
+		return min_index
+
+	# Function that implements Dijkstra's single source
+	# shortest path algorithm for a graph represented
+	# using adjacency matrix representation
+	def dijkstra(self, src):
+
+		dist = [1e7] * self.V
+		dist[src] = 0
+		sptSet = [False] * self.V
+
+		for cout in range(self.V):
+
+			# Pick the minimum distance vertex from
+			# the set of vertices not yet processed.
+			# u is always equal to src in first iteration
+			u = self.minDistance(dist, sptSet)
+
+			# Put the minimum distance vertex in the
+			# shortest path tree
+			sptSet[u] = True
+
+			# Update dist value of the adjacent vertices
+			# of the picked vertex only if the current
+			# distance is greater than new distance and
+			# the vertex in not in the shortest path tree
+			for v in range(self.V):
+				if (self.graph[u][v] > 0 and
+				sptSet[v] == False and
+				dist[v] > dist[u] + self.graph[u][v]):
+					dist[v] = dist[u] + self.graph[u][v]
+
+		self.printSolution(dist)
+
+# Driver program
+g = Graph(10)
+
+g.graph = [[0, 1.78, 0.84, 1.60, 1.08, 0, 0, 0, 2.00, 0],
+           [1.78, 0, 1.36, 0, 1.60, 0.49, 0, 0, 0, 0],
+           [0.84, 1.36, 0, 1.27, 0, 0, 1.29, 0, 0, 0],
+           [1.60, 0, 1.27, 0, 0, 0, 0, 1.64, 1.01, 0.75],
+           [1.08, 1.60, 0, 0, 0, 0, 0, 0, 0, 0],
+           [0, 0.49, 0, 0, 0, 0, 1.19, 0, 0, 0],
+           [0, 0, 1.29, 0, 0, 1.19, 0, 0, 0, 1.02],
+           [0, 0, 0, 1.64, 0, 0, 0, 0, 2.02, 1.38],
+           [2.00, 0, 0, 1.01, 0, 0, 0, 2.02, 0, 0],
+           [0, 0, 0, 0.75, 0, 0, 1.02, 1.38, 0, 0]
+           ]
+
+g.dijkstra(4)
 
 
-n = int(input('Podaj ile węzłów ma graf\n n = '))
-s = int(input('Podaj węzeł startowy\n s = '))
-print('Podaj elementy macierzy:')
-macierz = []
-for i in range(0, n):
-    tab = [int(x) for x in input().split()]
-    macierz.append(tab)
 
-tab = Dijkstra(macierz, s)
-for i in range(0, n):
-    wypiszDane(i, tab[i])
-def wypiszDane(i, d):
-  txt = str(i) + "\t"
-  if (not d.odwiedzony):
-    txt += "nieodwiedzony"
-  else:
-    if (d.poprzednik == -1):
-      txt += "brak"
-    else:
-      txt += str(d.poprzednik)
-    txt += "\t" + str(d.dystans)
-  print(txt);
+
 
 
 # Część 5:
