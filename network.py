cd5cd775c7d0c84786bb7e647068ffe5a571c627
Julek <jkluge>
2022-11-19 19:20:28 +0100

    1

--- network.py
+++ network.py
@@ -7,6 +7,7 @@ response = requests.get("http://192.168.8.166:8181/onos/v1/flows" , auth=("karaf
 response2 = requests.get("http://192.168.8.166:8181/onos/v1/hosts" , auth=("karaf", "karaf"))
 response3 = requests.get("http://192.168.8.166:8181/onos/v1/links" , auth=("karaf", "karaf"))
 response4 = requests.get("http://192.168.8.166:8181/onos/v1/devices" , auth=("karaf", "karaf"))
+response5 = requests.get("http://192.168.8.166:8181/onos/v1/flows/of:0000000000000002" , auth=("karaf", "karaf"))
 
 if (response.status_code == 200):
     print("The request was a success!")
@@ -33,6 +34,15 @@ if (response4.status_code == 200):
     print(response4.json())
 elif (response4.status_code == 404):
     print("Result not found!")
+
+if (response5.status_code == 200):
+    print("The request was a success!")
+    print(response5.json())
+elif (response5.status_code == 404):
+    print("Result not found!")
+
+
+
 # odpowiedni sposób przechowania informacji o węzłach i łączach
 
 
