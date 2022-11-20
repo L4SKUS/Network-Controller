db30f13f4bfe6425e0e91aa165893b304a735d5c
Julek <jkluge>
2022-11-20 01:19:50 +0100

    1

--- djwd.py
+++ djwd.py
@@ -3,26 +3,19 @@
 import math
 
 graph = {
-    'a': {'b': 5, 'c': 2},
-    'b': {'a': 5, 'c': 7, 'd': 8},
-    'c': {
-        'a': 2,
-        'b': 7,
-        'd': 4,
-        'e': 8,
-        },
-    'd': {
-        'b': 8,
-        'c': 4,
-        'e': 6,
-        'f': 4,
-        },
-    'e': {'c': 8, 'd': 6, 'f': 3},
-    'f': {'e': 3, 'd': 4},
+    'Warszawa': {'Krakow': 1.78, 'lodz': 0.84, 'Bydgoszcz':1.6,'Lublin':1.08,'Gdansk':2},
+    'Krakow': {'Katowice': 0.49, 'Lodz': 1.36, 'Lublin': 1.6},
+    'Lodz': {'Warszawa': 0.84,'Krakow': 1.36,'Wroclaw': 1.29,'Bydgoszcz': 1.27},
+    'Bydgoszcz': {'Warszawa': 1.6,'Lodz': 1.27,'Gdansk': 1.01,'Poznan': 0.75,'Szczecin':1.64},
+    'Katowice': {'Krakow': 0.49, 'Wroclaw': 1.19},
+    'Wroclaw': {'Katowice': 1.19, 'Lodz': 1.29, 'Poznan': 1.02},
+    'Szczecin': {'Poznan': 3, 'Bydgoszcz': 1.64, 'Gdansk': 2.02},
+    'Gdansk': {'Bydgoszcz': 1.01, 'Szczecin': 2.02,'Warszawa':2},
+    'Poznan': {'Bydgoszcz': 0.75, 'Wroclaw': 1.02, 'Szczecin':1.38},
     }
 
-source = 'a'
-destination = 'f'
+source = 'Lodz'
+destination = 'Gdansk'
 
 unvisited = graph
 shortest_distances = {}
